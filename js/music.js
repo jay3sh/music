4fd416576ad4dbffe9546dcedadd75dfb53b5d73
Nihar Sawant <nihar@3dtin.com>
2011-10-14 13:48:08 +0530

    seperate class for player

--- js/music.js
+++ js/music.js
@@ -1,13 +1,13 @@
 
 google.load('search', '1');
 
-function player_init(){
+function skin_init(){
   var width = (($(document).width())>1280? 1280: $(document).width());
   $.app.canvas = $('#seeker').get(0);
   $.app.ctx = $.app.canvas.getContext('2d');
   $('body').css('width', width);
   $('#player').get(0).volume = 0.1;
-  $.app.playerAction = 'play';
+  $.app.player.action = 'play';
 
   $('#player_wrapper #album_artwork').attr('src', '/images/nothumb.png');
   google.search.Search.getBranding('google_branding');
@@ -135,184 +135,17 @@ function searchImage(query, callback) {
   imageSearch.execute(query);
 }
 
-function updateSongInfo(muFile){
-  var songInfo = $('#player_column #player_controls #song_info');
-  var yt = '<a href='+getYoutubeSearchURL(muFile)+' target="blank"><img src="/images/youtube.ico" /></a>';
-  var wiki = '<a href='+getArtistWikipediaURL(muFile)+' target="blank"><img src="/images/wikipedia.ico" /></a>';
-  var amzn = '<a href='+getAmazonLink(muFile)+' target="blank"><img src="/images/amazon.png" /></a>';
-  var lyrics = '<a href='+getMetroLyricsURL(muFile)+' target="blank"><img src="/images/metro.png" /><a/>';
-  $('#title', songInfo).text(muFile.title).attr('title', muFile.title);
-  $('#artist', songInfo).text(muFile.artist).attr('title', muFile.artist);
-  $('#album', songInfo).text(muFile.album).attr('title', muFile.album);
-  $('#extern', songInfo).html(yt+wiki+amzn+lyrics);
-  
-}
 
-var currentPlaying = null;
-$.app.prevSong = function () {
-  var prevDiv = currentPlaying.prev();
-  if(prevDiv.length == 0) {
-    prevDiv = currentPlaying.parent().children().last();
-  }
-  playMedia(prevDiv);
-}
-
-$.app.nextSong = function () {
-  var nextDiv = currentPlaying.next();
-  if(nextDiv.length == 0) {
-    nextDiv = currentPlaying.parent().children().first();
-  }
-  playMedia(nextDiv);
-}
-
-function animateSeeker() {
-    var player = $('#player').get(0);
-
-    var degree = (player.currentTime/player.duration)*360;
-
-    $.app.ctx.beginPath();  
-    $.app.ctx.clearRect(0,0,$.app.canvas.width,$.app.canvas.height);
-    $.app.ctx.strokeStyle = 'rgba(00, 194, 256, 0.9)';
-    $.app.ctx.lineWidth = 14;
-    $.app.ctx.arc(75,75,65,Math.PI*(270/180),Math.PI*(degree+270)/180);
-    $.app.ctx.shadowOffsetX = 0;
-    $.app.ctx.shadowOffsetY = 0;
-    $.app.ctx.shadowBlur = 5;
-    $.app.ctx.shadowColor = 'rgba(170,170,170,0.7)';
-    $.app.ctx.stroke();
-
-    $.app.ctx.closePath();
-
-    setTimeout(animateSeeker, 1000);
-}
-
-function pauseMedia() {
-  $('#player').get(0).pause();
-}
-
-function playMedia(div, resumeFlag) {
-  if(!div){
-    if($('#playlist', '#playlist_wrapper').is(':empty')){
-      alert('No selction made.');
-      return;
-    } else { 
-      div = $('.entry', '#playlist').first();
-      resumeFlag = false;
-    }
-  }
-  var muFile = div.find('.entry_action').data('muFile');
-  var url = getObjectURL(muFile.path);
-  
-  if(url) {
-    if(!resumeFlag) { $('#player').get(0).src = url; }
-
-    var wrapper_height = $('#player_wrapper').height()-30;
-    var wrapper_width = $('#player_wrapper').width()-30;
-
-    $('#player').get(0).play();
-    currentPlaying = div;
-    currentPlaying.parent().children().removeClass('active_entry');
-    currentPlaying.addClass('active_entry');
-    searchImage(muFile.album+' '+muFile.artist,function (tbUrl) {
-      $('#player_wrapper #album_artwork').attr('src',tbUrl);
-      var height = $('#player_wrapper #album_artwork').height();
-      var width = $('#player_wrapper #album_artwork').width();
-
-      if(height > wrapper_height || width > wrapper_width){
-        var ratio, new_height, new_width;
-        if(height > wrapper_height){
-          ratio = width/height;
-          new_height = wrapper_height;
-          new_width = ratio*new_height;
-        } else {
-          ratio = height/width;
-          new_width = wrapper_width;
-          new_height = ratio*new_width;
-        } 
-        $('#player_wrapper img').css('height', new_height);
-        $('#player_wrapper img').css('width', new_width);
-      }
-
-    });
-    updateSongInfo(muFile);
-  } else {
-    alert('Add music again');
-  }
-}
 
 $(document).ready(function () {
-  player_init();
+  skin_init();
+  $.app.player.init();
   $.app.Storage.load();
 
   $('#addmusic').click(function () {
     $('input[name=actual_addmusic]').click();
   });
 
-  $('#play', '#player_column')
-    .hover(
-      function () {
-        $('img', this)
-          .attr('src', '/images/'+$.app.playerAction+'_hover.png');
-      },
-      function () {       
-        $('img', this)
-          .attr('src', '/images/'+$.app.playerAction+'.png');
-      }
-    )
-    .click(function () {
-      if($.app.playerAction == 'play'){
-        playMedia(currentPlaying, true);
-      } else {
-        pauseMedia();
-      } 
-    });
-
-  $('#prev', '#player_column')
-    .hover(
-      function (){
-        $('img', this).attr('src', '/images/previous_hover.png');
-      }, 
-      function (){
-        $('img', this).attr('src', '/images/previous.png');
-      }
-    )
-    .click(function (){
-      $.app.prevSong(); 
-    });
-
-  $('#next', '#player_column')
-    .hover(
-      function (){
-        $('img', this).attr('src', '/images/next_hover.png');
-      }, 
-      function (){
-        $('img', this).attr('src', '/images/next.png');
-      }
-    )
-    .click(function (){
-      $.app.nextSong(); 
-    });
-
-  $('#player', '#player_column')
-    .bind('play', function () {
-      $.app.playerAction = 'pause';
-      $('img', '#play').attr('src', '/images/pause.png');
-      animateSeeker();
-    })
-    .bind('pause', function () {
-      $.app.playerAction = 'play';
-      $('img', '#play').attr('src', '/images/play.png');
-    });
-
-  $('#player_volume').slider({
-    'range' : 'min',
-    'value' : 10,
-    'min' : 0.0,
-    'max' : 100,
-    'slide' : function (e, ui){
-      $('#player').get(0).volume = (ui.value)/100;
-    }
-  });
   $('input[name=search]').keyup(function (e) {
 
     var divResults = $('#search_results');
@@ -331,10 +164,10 @@ $(document).ready(function () {
         var muFile = $(this).data('muFile');
         var player_entry = getSongEntryHtml(muFile, false);
         player_entry.find('.entry_action').click(function () {
-          playMedia($(this).parent());
+          $.app.player.playMedia($(this).parent());
         });
         player_entry.find('.remove_action').click(function () {
-          playMedia($(this).parent().remove());
+          $.app.player.playMedia($(this).parent().remove());
         });
         $('#playlist_wrapper #playlist').append(player_entry);
       });
---
+++ js/player.js
@@ -0,0 +1,182 @@
+(function ($){
+var app = $.app;
+
+function player () {}
+
+function updateSongInfo(muFile){
+  var songInfo = $('#player_column #player_controls #song_info');
+  var yt = '<a href='+getYoutubeSearchURL(muFile)+' target="blank"><img src="/images/youtube.ico" /></a>';
+  var wiki = '<a href='+getArtistWikipediaURL(muFile)+' target="blank"><img src="/images/wikipedia.ico" /></a>';
+  var amzn = '<a href='+getAmazonLink(muFile)+' target="blank"><img src="/images/amazon.png" /></a>';
+  var lyrics = '<a href='+getMetroLyricsURL(muFile)+' target="blank"><img src="/images/metro.png" /><a/>';
+  $('#title', songInfo).text(muFile.title).attr('title', muFile.title);
+  $('#artist', songInfo).text(muFile.artist).attr('title', muFile.artist);
+  $('#album', songInfo).text(muFile.album).attr('title', muFile.album);
+  $('#extern', songInfo).html(yt+wiki+amzn+lyrics);
+  
+}
+
+function animateSeeker() {
+  var player = $('#player').get(0);
+
+  var degree = (player.currentTime/player.duration)*360;
+
+  $.app.ctx.beginPath();  
+  $.app.ctx.clearRect(0,0,$.app.canvas.width,$.app.canvas.height);
+  $.app.ctx.strokeStyle = 'rgba(00, 194, 256, 0.9)';
+  $.app.ctx.lineWidth = 14;
+  $.app.ctx.arc(75,75,65,Math.PI*(270/180),Math.PI*(degree+270)/180);
+  $.app.ctx.shadowOffsetX = 0;
+  $.app.ctx.shadowOffsetY = 0;
+  $.app.ctx.shadowBlur = 5;
+  $.app.ctx.shadowColor = 'rgba(170,170,170,0.7)';
+  $.app.ctx.stroke();
+
+  $.app.ctx.closePath();
+
+  setTimeout(animateSeeker, 1000);
+}
+
+player.prevSong = function () {
+  var prevDiv = this.currentPlaying.prev();
+  if(prevDiv.length == 0) {
+    prevDiv = this.currentPlaying.parent().children().last();
+  }
+  this.playMedia(prevDiv);
+}
+
+player.nextSong = function () {
+  var nextDiv = this.currentPlaying.next();
+  if(nextDiv.length == 0) {
+    nextDiv = this.currentPlaying.parent().children().first();
+  }
+  this.playMedia(nextDiv);
+}
+
+player.pauseMedia = function () {
+  $('#player').get(0).pause();
+}
+
+player.playMedia = function (div, resumeFlag) {
+  if(!div){
+    if($('#playlist', '#playlist_wrapper').is(':empty')){
+      alert('No selction made.');
+      return;
+    } else { 
+      div = $('.entry', '#playlist').first();
+      resumeFlag = false;
+    }
+  }
+  var muFile = div.find('.entry_action').data('muFile');
+  var url = getObjectURL(muFile.path);
+  
+  if(url) {
+    if(!resumeFlag) { $('#player').get(0).src = url; }
+
+    var wrapper_height = $('#player_wrapper').height()-30;
+    var wrapper_width = $('#player_wrapper').width()-30;
+
+    $('#player').get(0).play();
+    this.currentPlaying = div;
+    this.currentPlaying.parent().children().removeClass('active_entry');
+    this.currentPlaying.addClass('active_entry');
+    searchImage(muFile.album+' '+muFile.artist,function (tbUrl) {
+      $('#player_wrapper #album_artwork').attr('src',tbUrl);
+      var height = $('#player_wrapper #album_artwork').height();
+      var width = $('#player_wrapper #album_artwork').width();
+
+      if(height > wrapper_height || width > wrapper_width){
+        var ratio, new_height, new_width;
+        if(height > wrapper_height){
+          ratio = width/height;
+          new_height = wrapper_height;
+          new_width = ratio*new_height;
+        } else {
+          ratio = height/width;
+          new_width = wrapper_width;
+          new_height = ratio*new_width;
+        } 
+        $('#player_wrapper img').css('height', new_height);
+        $('#player_wrapper img').css('width', new_width);
+      }
+
+    });
+    updateSongInfo(muFile);
+  } else {
+    alert('Add music again');
+  }
+}
+
+player.init = function () {
+  var thisref = this;
+  thisref.currentPlaying = null;
+
+  $('#play', '#player_column')
+    .hover(
+      function () {
+        $('img', this)
+          .attr('src', '/images/'+player.action+'_hover.png');
+      },
+      function () {       
+        $('img', this)
+          .attr('src', '/images/'+player.action+'.png');
+      }
+    )
+    .click(function () {
+      if(player.action == 'play'){
+        thisref.playMedia(thisref.currentPlaying, true);
+      } else {
+        thisref.pauseMedia();
+      } 
+    });
+
+  $('#prev', '#player_column')
+    .hover(
+      function (){
+        $('img', this).attr('src', '/images/previous_hover.png');
+      }, 
+      function (){
+        $('img', this).attr('src', '/images/previous.png');
+      }
+    )
+    .click(function (){
+      thisref.prevSong(); 
+    });
+
+  $('#next', '#player_column')
+    .hover(
+      function (){
+        $('img', this).attr('src', '/images/next_hover.png');
+      }, 
+      function (){
+        $('img', this).attr('src', '/images/next.png');
+      }
+    )
+    .click(function (){
+      thisref.nextSong(); 
+    });
+
+  $('#player', '#player_column')
+    .bind('play', function () {
+      player.action = 'pause';
+      $('img', '#play').attr('src', '/images/pause.png');
+      animateSeeker();
+    })
+    .bind('pause', function () {
+      player.action = 'play';
+      $('img', '#play').attr('src', '/images/play.png');
+    });
+
+  $('#player_volume').slider({
+    'range' : 'min',
+    'value' : 10,
+    'min' : 0.0,
+    'max' : 100,
+    'slide' : function (e, ui){
+      $('#player').get(0).volume = (ui.value)/100;
+    }
+  });
+};
+
+app.player = player;  
+})(jQuery)
