91a0ed2be61c320065b64ea1c20a68081104827b
Nihar Sawant <nihar@3dtin.com>
2011-10-15 16:28:42 +0530

    setting song_info width dynamically

--- js/music.js
+++ js/music.js
@@ -9,6 +9,9 @@ function skin_init(){
     $('#search_column').height() - $('#player_wrapper').height();
   $('#playlist_wrapper').css('height', playlistHeight);
 
+  var playerColumnWidth = $('#player_column').width()/4;
+  $('#song_info').children().css('width', playerColumnWidth);
+
   $.app.canvas = $('#seeker').get(0);
   $.app.ctx = $.app.canvas.getContext('2d');
   google.search.Search.getBranding('google_branding');
