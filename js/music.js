352ec5b290a51bdacb1cee50ca4b6572d0acd371
Nihar Sawant <nihar@3dtin.com>
2011-10-15 13:10:52 +0530

    keep 1280 and above as default resolution

--- js/music.js
+++ js/music.js
@@ -2,7 +2,7 @@
 google.load('search', '1');
 
 function skin_init(){
-  var width = (($(document).width())>1280? 1280: $(document).width());
+  var width = ($(document).width()>1280 ? $(document).width() : 1280);
   $.app.canvas = $('#seeker').get(0);
   $.app.ctx = $.app.canvas.getContext('2d');
   $('body').css('width', width);
