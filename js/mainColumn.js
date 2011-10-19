c75dffb6dcfcbadb0397e1cc4406f1f607f220ad
Nihar Sawant <nihar@3dtin.com>
2011-10-19 14:06:30 +0530

    putting input keyhandler inside mainColumn
    since its a part of searchColumn its inside mainColumn.
    
    two seperate functions are written which will take care of toggle of various modes

--- js/mainColumn.js
+++ js/mainColumn.js
@@ -3,6 +3,16 @@
   var app = $.app;
 
   function mainColumn () {}
+
+  function showSearchResults() {
+    $('#shelf', '#search_column').hide();
+    $('#search_results', '#search_column').show();
+  }
+
+  function showShelf() {
+    $('#shelf', '#search_column').show();
+    $('#search_results', '#search_column').hide();
+  }
   
   function loadMusic(files) {
     var musicFiles = _(files).select(function (file) {
@@ -16,6 +26,7 @@
         (/\.m4a$/.test(lpath))
     });
 
+    showShelf();
     var artworkHints = [];
     var total = musicFiles.length, progress = 0;
     var shelf = $('#shelf', '#search_column');
@@ -73,8 +84,41 @@
   }
   
   function init() {
-    if(_.isNull(window.localStorage.getItem('__name_index__'))){} 
-    else { } 
+    /*if(_.isNull(window.localStorage.getItem('__name_index__'))){} 
+    else { } */ 
+    $('input[name=search]').keyup(function (e) {
+
+      var divResults = $('#search_results');
+      divResults.empty();
+      var text = $(this).val();
+
+      if(!text || (text && text.length < 4)) { return; }
+
+      var results = $.app.Storage.search($(this).val());
+      _(results).each(function (muFile) {
+        divResults.append($.app.utils.getSongEntryHTML(muFile, true));
+      });
+
+
+      divResults.find('.entry_action')
+        .click(function () {
+          var muFile = $(this).data('muFile');
+          var player_entry = $.app.utils.getSongEntryHTML(muFile, false);
+          $.app.Playlist.attachEntryControls(player_entry);
+          $.app.Playlist.add(player_entry)
+        });
+
+    })
+    .focus(function (e){
+      $(this).val('');
+      showSearchResults();
+    })
+    .focusout(function (e){
+      if($(this).val() == ''){
+        $(this).val('Search...');
+        showShelf();
+      }
+    }); 
   }
 
   app.mainColumn = mainColumn;
--- js/music.js
+++ js/music.js
@@ -24,10 +24,10 @@ function skin_init(){
 
 
 $(document).ready(function () {
-  $.app.mainColumn.init();
   $.app.player.init($('#player_column'));
   $.app.Storage.load();
   $.app.Playlist.loadPlaylist();
+  $.app.mainColumn.init();
   $.app.Cache.init();
   skin_init();
 
@@ -35,37 +35,6 @@ $(document).ready(function () {
     $('input[name=actual_addmusic]').click();
   });
 
-  $('input[name=search]').keyup(function (e) {
-
-    var divResults = $('#search_results');
-    divResults.empty();
-    var text = $(this).val();
-
-    if(!text || (text && text.length < 4)) { return; }
-
-    var results = $.app.Storage.search($(this).val());
-    _(results).each(function (muFile) {
-      divResults.append($.app.utils.getSongEntryHTML(muFile, true));
-    });
-
-
-    divResults.find('.entry_action')
-      .click(function () {
-        var muFile = $(this).data('muFile');
-        var player_entry = $.app.utils.getSongEntryHTML(muFile, false);
-        $.app.Playlist.attachEntryControls(player_entry);
-        $.app.Playlist.add(player_entry)
-      });
-
-  })
-  .focus(function (e){
-    $(this).val('');
-  })
-  .focusout(function (e){
-    if($(this).val() == ''){
-      $(this).val('Search...');
-    }
-  }); 
 });
 
 window.onbeforeunload =  function () {
