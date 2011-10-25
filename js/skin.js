7988316d66e54f67232bc2d72649e855e2e31862
Nihar Sawant <nihar@3dtin.com>
2011-10-25 19:23:09 +0530

    reset skin for tabs

--- js/skin.js
+++ js/skin.js
@@ -3,35 +3,33 @@
   
   function Skin() {}
   
-  function init(){
-    var width = ($(document).width()>1280 ? $(document).width() : 1280);
-    $('body').css('width', width);
-
+  function init() {
     var playlistHeight = 
-      $('#search_column').height() - $('#player_wrapper').height();
-    $('#playlist_wrapper').css('height', playlistHeight);
+      $(document).height() - $('#player_column').height() - $('#footer').height() - 60;
+    $('#playlist_wrapper').height(playlistHeight);
     
-    var playerColumnWidth = $('#player_column').width()-
+    $('#tabs').tabs();
+    /*var playerColumnWidth = $('#player_column').width()-
       ($('#player_skin').width()+$('#player_wrapper').width()+50);
     $('#song_info').children().css('width', playerColumnWidth);
     $('.title').css('width', playerColumnWidth-55);
 
     $('input[name=search]')
       .width($('#search_column').width()-$('#addall').width()-40);
-   
+    */
     $.app.canvas = $('#seeker').get(0);
     $.app.ctx = $.app.canvas.getContext('2d');
 
-    $('#progressbar').width($('#shelf', '#search_column').width());
+    /*$('#progressbar').width($('#shelf', '#search_column').width());
     $('#progressbar').height($('#shelf', '#search_column').height());
     $.app.progress_canvas = $('#complete_progress').get(0);
     $.app.progress_ctx = $.app.progress_canvas.getContext('2d');
-
+    */
     google.search.Search.getBranding('google_branding');
     $('.gsc-branding-text', '#google_branding')
       .text('Artworks powered by ')
       .css({'font-size' : '11px', 'text-align' : 'right'});
-
+    
     $('#addmusic').click(function () {
       $('input[name=actual_addmusic]').click();
     });
