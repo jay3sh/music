fd548825b9b3624b9de0838902933d98290d4113
Jayesh Salvi <jayesh@3dtin.com>
2011-10-29 11:36:09 +0530

    Cache expiration of LRU entries
    (fixes #10)
    
    The cached song files are now saved with timestamp appended to them.
    When QUOTA_EXCEEDED_ERR is thrown, deleteLRU function is called, which
    sorts all existing cached entries according to their timestamp and
    deletes the first one (one with oldest timestamp). This process gets
    repeated recursively if deleting single entry does not free up
    sufficient space.

--- js/cache.js
+++ js/cache.js
@@ -35,44 +35,77 @@ function parseError(e) {
 }
 
 Cache.getPlayableURL = function (hash, callback) {
-  window.requestFileSystem(window.TEMPORARY, CACHE_SIZE,
-    function onSuccess(fs) {
-      fs.root.getDirectory('songs', { create:true }, function (dirEntry) {
-          dirEntry.getFile(hash, { create:false },
-          function success(file) { callback(file.toURL()); },
-          function failure(e) { callback(null); });
-      }, function (e) {
-        console.error('getDir: '+parseError(e));
-        callback(null);
-      });
-    }, function (e) {
-      console.error('reqfs: '+parseError(e)); 
-      callback(null);
+  Cache.list('songs', function (entries) {
+    var matching = _(entries).select(function (fe) {
+      var match = /(\w+)\.(\w+)/.exec(fe.name)
+      return match[1] == hash;
     });
+    if(matching.length > 0) {
+      if(callback) { callback(matching[0].toURL()); }
+    } else {
+      callback(null);
+    }
+  }, function (e) {
+    console.log('Cache.list '+parseError(e));
+    callback(null);
+  });
 };
 
 Cache.add = function (hash, f) {
   window.requestFileSystem(window.TEMPORARY, CACHE_SIZE,
     function onSuccess(fs) {
-      fs.root.getDirectory('songs', {},
-        function (dirEntry) {
-          dirEntry.getFile(hash, { create:true },
-          function success(fileEntry) {
-            fileEntry.createWriter(
-              function(fileWriter) { fileWriter.write(f); },
-              function (e) { console.error('write: '+parseError(e)); }
-            );
+
+      (function addEntry() {
+        fs.root.getDirectory('songs', {},
+          function (dirEntry) {
+            console.log('Adding to cache', hash);
+            dirEntry.getFile(hash+'.'+new Date().getTime(), { create:true },
+            function success(fileEntry) {
+              fileEntry.createWriter(
+                function(fileWriter) { fileWriter.write(f); },
+                function (e) { console.error('write: '+parseError(e)); }
+              );
+            },
+            function failure(e) {
+              if(e.code == FileError.QUOTA_EXCEEDED_ERR) {
+                Cache.deleteLRU(addEntry);
+              } else {
+                console.error('getFile: '+parseError(e));
+              }
+            });
           },
-          function failure(e) { console.error('getFile: '+parseError(e)); });
-        },
-        function (e) { console.error('getDir: '+parseError(e)); }
-      );
+          function (e) { console.error('getDir: '+parseError(e)); }
+        );
+      })()
+
     },
     function (e) { console.log('reqfs: '+parseError(e)); }
   );
 };
 
-Cache.list = function (dirname, callback) {
+Cache.deleteLRU = function (callback, errcallback) {
+  Cache.list('songs',function (entries) {
+    if(entries.length == 0) {
+      throw new Exception('Unexpected call to deleteLRU');
+    }
+    var timedEntries = _(entries).map(function (fe) {
+      var match = /(\w+)\.(\w+)/.exec(fe.name);
+      return { timestamp : parseInt(match[2], 10), entry : fe };
+    });
+    timedEntries.sort(function (x, y) {
+      return x.timestamp > y.timestamp;
+    });
+    console.log('Removing LRU '+timedEntries[0].entry.name);
+    timedEntries[0].entry.remove(
+      function () { callback(); },
+      function (e) { if(errcallback) { errcallback(e); } }
+    );
+  }, function (e) {
+    errcallback(e);
+  });
+}
+
+Cache.list = function (dirname, callback, errcallback) {
   window.requestFileSystem(window.TEMPORARY, CACHE_SIZE,
     function onSuccess(fs) {
       fs.root.getDirectory(dirname, {},
@@ -91,13 +124,13 @@ Cache.list = function (dirname, callback) {
                 readEntries();
               }
             },
-            function (e) {console.error('readEntries '+parseError(e));});
+            function (e) { if(errcallback) { errcallback(e); } });
           })();
         },
-        function (e) { console.error('getDir: '+parseError(e)); }
+        function (e) { if(errcallback) { errcallback(e); } }
       );
     },
-    function (e) { console.log('reqfs: '+parseError(e)); }
+    function (e) { if(errcallback) { errcallback(e); } }
   );
 };
 
@@ -108,6 +141,9 @@ Cache.empty = function () {
         console.error('file delete', parseError(e));
       });
     });
+  },
+  function (e) {
+    console.error('Cache.list '+parseError(e));
   })
 };
 
